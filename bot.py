bb8428f024aff8eec0fcdaf19d523cd091c54cb2
Fabricio Covalesci <fabcovalesci@gmail.com>
2021-04-13 23:31:31 -0300

    init

---
+++ bot.py
@@ -0,0 +1,42 @@
+from binance.client import Client
+from binance.enums import *
+from tradingview_ta import TA_Handler, Interval, Exchange
+import datetime
+import random
+import time
+from datetime import datetime
+import config
+from sendEmail import SendEmail
+
+client = Client(config.API_KEY, config.API_SECRET, tld='com')
+
+
+while 1:
+
+    now = datetime.now()
+    fecha = now.strftime("%d-%m-%y %H:%M:%S")
+    lista = client.get_all_tickers()
+    strongBuy_list = []
+    strongSell_list = []
+    for i in lista:
+        tesla = TA_Handler()
+        tesla.set_symbol_as(i['symbol'])
+        tesla.set_exchange_as_crypto_or_stock("BINANCE")
+        tesla.set_screener_as_crypto()
+        tesla.set_interval_as(Interval.INTERVAL_1_HOUR)
+        print(i['symbol'])
+        try:
+        print(tesla.get_analysis().summary)
+        except Exception as e:
+        print("No Data")
+        continue
+        if((tesla.get_analysis().summary)["RECOMMENDATION"])=="STRONG_BUY":
+            print(f" COMPRA FORTE {i}", fecha)
+            strongBuy_list.append(i['symbol'])
+        elif((tesla.get_analysis().summary)["RECOMMENDATION"])=="STRONG_SELL":
+            print(f" VENDA FORTE {i}", fecha)
+            strongSell_list.append(i['symbol'])
+            
+    SendEmail(strongBuy_list, strongSell_list)
+
+    time.sleep(300)
---
+++ sendEmail.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from datetime import datetime
+import config
+
+
+
+def SendEmail(buy, sell):
+    mail_content = F'''Ola,
+        Abaixo segue a relação de criptomoedas, com a recomendação do Trading View
+
+        Forte venda: {sell}
+
+        Forte Compra: {buy}
+        '''
+    now = datetime.now()
+
+    sender_address = config.GMAIL_EMAIL
+    sender_pass = config.GMAIL_PASSWORD
+    receiver_address = 'fabcovalesci@gmail.com'
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = F'BOT - CRIPTOMOEDAS - {now}' 
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')

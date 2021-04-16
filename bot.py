04de485fab351cc9cef3baea003343f095181cd1
Fabricio Covalesci <fabcovalesci@gmail.com>
2021-04-15 21:59:06 -0300

    modificado data email

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from binance.client import Client
 from binance.enums import *
 from tradingview_ta import TA_Handler, Interval, Exchange
-import datetime
 import random
 import time
 from datetime import datetime
@@ -9,45 +8,6 @@ import config
 from sendEmail import SendEmail, SendEmailDeploy
 
 client = Client(config.API_KEY, config.API_SECRET, tld='com')
-
-
-# while 1:
-
-#     SendEmailDeploy()
-#     now = datetime.now()
-#     fecha = now.strftime("%d-%m-%y %H:%M:%S")
-#     lista = client.get_all_tickers()
-#     strongBuy_list = []
-#     strongSell_list = []
-#     for i in lista:
-#         tesla = TA_Handler()
-#         tesla.set_symbol_as(i['symbol'])
-#         tesla.set_exchange_as_crypto_or_stock("BINANCE")
-#         tesla.set_screener_as_crypto()
-#         tesla.set_interval_as(Interval.INTERVAL_1_HOUR)
-#         print(i['symbol'])
-#         try:
-#             print(tesla.get_analysis().summary)
-#         except Exception as e:
-#             print("No Data")
-#         continue
-#         if((tesla.get_analysis().summary)["RECOMMENDATION"])=="STRONG_BUY":
-#             print(f" COMPRA FORTE {i} | {len(strongBuy_list)}", fecha)
-#             strongBuy_list.append(i['symbol'])
-#         elif((tesla.get_analysis().summary)["RECOMMENDATION"])=="STRONG_SELL":
-#             print(f" VENDA FORTE {i} | {len(strongSell_list)}", fecha)
-#             strongSell_list.append(i['symbol'])
-    
-#     Buy = strongBuy_list
-#     Sell = strongSell_list
-
-#     print(F'Recomendacao de Compra {Buy}')
-
-#     print(F'Recomendacao de Venda {Sell}')
-    
-#     SendEmail(Buy, Sell)
-
-#     time.sleep(300)
 
 while True:
     
--- sendEmail.py
+++ sendEmail.py
@@ -19,17 +19,17 @@ def SendEmail(buy, sell):
     sender_address = config.GMAIL_EMAIL
     sender_pass = config.GMAIL_PASSWORD
     receiver_address = 'fabcovalesci@gmail.com'
-    #Setup the MIME
+
     message = MIMEMultipart()
     message['From'] = sender_address
     message['To'] = receiver_address
-    message['Subject'] = F'BOT - CRIPTOMOEDAS - {now}' 
-    #The body and the attachments for the mail
+    message['Subject'] = F'BOT - CRIPTOMOEDAS - {now.strftime("%d-%m-%y %H:%M:%S")}' 
+ 
     message.attach(MIMEText(mail_content, 'plain'))
-    #Create SMTP session for sending the mail
-    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-    session.starttls() #enable security
-    session.login(sender_address, sender_pass) #login with mail_id and password
+ 
+    session = smtplib.SMTP('smtp.gmail.com', 587) 
+    session.starttls()
+    session.login(sender_address, sender_pass) 
     text = message.as_string()
     session.sendmail(sender_address, receiver_address, text)
     session.quit()
@@ -45,17 +45,16 @@ def SendEmailDeploy():
     sender_address = config.GMAIL_EMAIL
     sender_pass = config.GMAIL_PASSWORD
     receiver_address = 'fabcovalesci@gmail.com'
-    #Setup the MIME
+    
     message = MIMEMultipart()
     message['From'] = sender_address
     message['To'] = receiver_address
-    message['Subject'] = F'BOT - CRIPTOMOEDAS - {now}' 
-    #The body and the attachments for the mail
+    message['Subject'] = F'BOT - CRIPTOMOEDAS - {now.strftime("%d-%m-%y %H:%M:%S")}' 
+
     message.attach(MIMEText(mail_content, 'plain'))
-    #Create SMTP session for sending the mail
-    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-    session.starttls() #enable security
-    session.login(sender_address, sender_pass) #login with mail_id and password
+    session = smtplib.SMTP('smtp.gmail.com', 587) 
+    session.starttls() 
+    session.login(sender_address, sender_pass)
     text = message.as_string()
     session.sendmail(sender_address, receiver_address, text)
     session.quit()

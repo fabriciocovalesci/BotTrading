719c4941d591e10ca3842c4d84839e5b5d533572
Fabricio Covalesci <fabcovalesci@gmail.com>
2021-04-18 17:05:04 -0300

    removido

--- config.py
+++
@@ -1,4 +0,0 @@
-API_KEY = 'PipcyZtskLcZxuOESeTi3p2WKVrlJimdjlpngTX6bODVPVZxriJ7uJlGT05s2tRd'
-API_SECRET = 'fo1OFsFuJFBNQaaCTwLRfHUn4HVldnem3BQTHOx6XaHZcYCioOutHIu6NvkacJN4'
-GMAIL_EMAIL = 'botfab42@gmail.com'
-GMAIL_PASSWORD = '$number42Bot'
